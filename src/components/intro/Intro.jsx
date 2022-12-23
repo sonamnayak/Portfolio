27fb930fffbf72bdb9cd882910b66532224dcf97
Sonam Nayak <sonamnayak2710@gmail.com>
2022-12-23 19:23:22 +0530

    Update Intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,4 @@
-import React, {useEffect, useRef} from 'react';
-import './intro.css';
+import React, {useEffect, useRef} from 'react'
 import { init } from 'ityped'
 
 const Intro = () => {
@@ -11,23 +10,23 @@ const Intro = () => {
 			showCursor: true,
 			backDelay: 1500,
 			backSpeed: 60,
-			strings: ["Full Stack", "MERN Stack"]
+			strings: ["MERN", "Full Stack"]
 		})
 	}, [])
 
 	return (
-		<div className="flex my-12 mx-4">
-			<div className="flex flex-1 flex-col justify-center pl-16 ">
-				<div className="text-4xl mb-4">Hello! I'm </div>
-				<div className="text-6xl mb-4 text-[#CB786C]">Sonam Nayak</div>
-				<div className="text-2xl">Web Developer <span ref={textRef} className="text-[#CB786C]"></span></div>
-				<a href="https://drive.google.com/file/d/1mJLt0vZpwZPoxbfT9TPuPEKUxtZXfdix/view?usp=sharing" className="button arrow w-[180px] text-sm px-2 py-3">Download Resume</a>
+		<div className="flex flex-col lg:flex-row mx-4 h-[93vh] pt-[7vh]">
+			<div className="flex flex-[0.4] md:flex-1 flex-col justify-center items-center lg:items-start lg:pl-16">
+				<div className="text-2xl md:text-4xl mb-4">Hello! I'm </div>
+				<div className="text-4xl md:text-6xl mb-6 md:mb-4 text-[#CB786C] text-center lg:text-left">Sonam Nayak</div>
+				<div className="text-lg md:text-2xl mb-6 md:mb-0">Web Developer <span ref={textRef} className="text-[#CB786C]"></span></div>
+				<a href="https://tinyurl.com/resumesonam" className="arrow w-[180px] text-sm px-2 py-3" target="_blank" rel="noreferrer">Download Resume</a>
 			</div>
-			<div className="flex-1">
-				<img className="w-[500px] h-[450px] m-auto bg-[#C5F2FB] mr-16" src="assests/intro.png" />
+			<div className="flex-[0.6] md:flex-1 flex justify-center" >
+				<img className="intro w-[300px] md:w-[550px] lg:h-[500px] m-auto bg-[#C5F2FB]" src="images/intro.png" alt="intro" />
 			</div>
 		</div>
-		);
+	)
 }
 
-export default Intro;
+export default Intro
