0a0e6e82612b0f9e33cd97452efe5a56377d0584
Sonam Nayak <sonamnayak2710@gmail.com>
2022-12-23 19:29:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
-import Intro from './components/intro/Intro';
-
-import Project from './components/projects/Project';
+import React from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Navbar from './components/navbar/Navbar'
+import Intro from './components/intro/Intro'
+import Education from './components/education/Education'
+import Project from './components/project/Project'
+import Certifications from './components/certifications/Certifications'
+import Experience from './components/experience/Experience'
+import Skills from './components/skills/Skills'
+import ScrollButton from './ScrollButton'
+import Footer from './components/footer/Footer'
 
 const App = () => {
     return (
@@ -11,9 +16,14 @@ const App = () => {
             <Navbar />
             <Intro />
             <Project />
+            {/* <Education /> */}
+            <Experience />
+            <Certifications />
+            <Skills />
+            <Footer />
+            <ScrollButton />
         </Router>
-        ); 
+        )
 }
 
-export default App;
-
+export default App
