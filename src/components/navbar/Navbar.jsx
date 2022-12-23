e9d540fbbe764d0f47110f5514bda862e0f414a4
Sonam Nayak <sonamnayak2710@gmail.com>
2022-12-23 19:24:16 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { IoMdMail } from 'react-icons/io'
+import { AiFillGithub, AiFillLinkedin, AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
-
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
 
     return (
-        <nav className='flex justify-between bg-[#CB786C] pl-[20px] pr-[20px]'>
-            <div>
+        <nav className='navbar flex justify-end md:justify-center lg:justify-end bg-[#CB786C] px-[30px] h-[7vh] fixed w-full z-20'>
+            <div className="dropdown my-auto">
+                <AiOutlineMenu size={30} className="text-white" onClick={() => setIsNavExpanded(!isNavExpanded)} />
             </div>
-            <div className='flex gap-8 my-4 justify-evenly my-4 text-white text-sm'>
+            <div className={isNavExpanded ? "nav expanded flex gap-6 my-auto text-lg md:text-base text-white" 
+                                          : "nav flex gap-6 my-auto text-lg md:text-base text-white"}>
+                <a href="#skills" className="pt-2 md:pt-0">SKILLS</a>
                 <a href="#project">PROJECTS</a>
-                <a href="#education">EDUCATION</a>
+                {/* <a href="#education">EDUCATION</a> */}
                 <a href="#experience">EXPERIENCE</a>
                 <a href="#certifications">CERTIFICATIONS</a>
-                <a href="#contact">CONTACT</a>
+                <a href="#">
+                    <div className="grid grid-cols-3 gap-x-3 text-3xl md:text-xl">
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=sonamnayak2710@gmail.com" target="_blank" rel="noreferrer"><IoMdMail /></a>
+                        <a href="https://github.com/sonamnayak/" target="_blank" rel="noreferrer"><AiFillGithub /></a>
+                        <a href="http://www.linkedin.com/in/sonamnayak/" target="_blank" rel="noreferrer"><AiFillLinkedin /></a>
+                    </div>
+                </a>
             </div>
         </nav>
-    );
+    )
 }
 
-export default Navbar;
+export default Navbar
